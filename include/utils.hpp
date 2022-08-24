5005381451a74fde28d82fee1564e2f96838ddef
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-08-24 23:47:58 +0200

    Add utils module

---
+++ include/utils.hpp
@@ -0,0 +1,13 @@
+#ifndef RAYTRACER_UTILS_H
+#define RAYTRACER_UTILS_H
+
+#include <limits>
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double PI = 3.1415926535897932385;
+
+double degToRad(double deg);
+
+double radToDeg(double rad);
+
+#endif
---
+++ src/utils.cpp
@@ -0,0 +1,9 @@
+#include "utils.hpp"
+
+double degToRad(double deg) {
+	return deg * PI / 180.0;
+}
+
+double radToDeg(double rad) {
+	return rad * 180.0 / PI;
+}
