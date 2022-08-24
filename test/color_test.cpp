b75af7c806fae93f0293a35d17fc5c5ca54da393
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-08-24 11:59:33 +0200

    Finish Color module's tests

--- test/color_test.cpp
+++ test/color_test.cpp
@@ -4,7 +4,7 @@
 
 #include "color.hpp"
 
-Color_black_test() {
+void Color_black_test() {
 	std::cout << "Testing Color black generator...\n";
 
 	assert(Color::black() == Color(0.0, 0.0, 0.0));
@@ -12,7 +12,7 @@ Color_black_test() {
 	std::cout << "Color black generator works!\n";
 }
 
-Color_white_test() {
+void Color_white_test() {
 	std::cout << "Testing Color white generator...\n";
 
 	assert(Color::white() == Color(0.0, 0.0, 0.0));
@@ -20,7 +20,7 @@ Color_white_test() {
 	std::cout << "Color white generator works!\n";
 }
 
-Color_equals_operator_test() {
+void Color_equals_operator_test() {
 	std::cout << "Testing Color \"==\" operator...\n";
 
 	assert(Color(0.23, 0.43, 0.92) == Color(0.23, 0.43, 0.92));
@@ -32,7 +32,7 @@ Color_equals_operator_test() {
 	std::cout << "Color \"==\" operator works!\n";
 }
 
-Color_not_equals_operator_test() {
+void Color_not_equals_operator_test() {
 	std::cout << "Testing Color \"!=\" operator...\n";
 
 	assert(Color(0.53, 0.43, 0.92) != Color(0.23, 0.43, 0.92));
@@ -44,7 +44,7 @@ Color_not_equals_operator_test() {
 	std::cout << "Color \"!=\" operator works!\n";
 }
 
-Color_addition_operator_test() {
+void Color_addition_operator_test() {
 	std::cout << "Testing Color \"+\" operator...\n";
 
 	assert(Color(0.25, 0.5, 1.0) + Color(-0.25, 0.5, -1.0) == Color(0.0, 1.0, 0.0));
@@ -58,7 +58,7 @@ Color_addition_operator_test() {
 	std::cout << "Color \"+\" operator works!\n";
 }
 
-Color_subtraction_operator_test() {
+void Color_subtraction_operator_test() {
 	std::cout << "Testing Color \"-\" operator...\n";
 
 	assert(Color(0.25, 0.5, 1.0) - Color(-0.25, 0.5, -1.0) == Color(0.5, 0.0, 2.0));
@@ -72,14 +72,66 @@ Color_subtraction_operator_test() {
 	std::cout << "Color \"-\" operator works!\n";
 }
 
-Color_multiplication_by_scalar_test() {
+void Color_multiplication_by_scalar_test() {
+	std::cout << "Testing Color \"*\" operator...\n";
+
+	assert(Color(0.0, 0.25, 0.5) * 2.0 == Color(0.0, 0.5, 1.0));
+	assert(2.0 * Color(0.0, 0.25, 0.5) == Color(0.0, 0.5, 1.0));
+
+	std::cout << "Vec3 \"*\" operator works!\n";
 }
 
-Color_division_by_scalar_test();
-Color_addition_assignment_operator_test();
-Color_subraction_assignment_operator_test();
-Color_multiplication_by_scalar_assignment_test();
-Color_division_by_scalar_assignment_test();
+void Color_division_by_scalar_test() {
+	std::cout << "Testing Color \"/\" operator...\n";
+
+	assert(Color(0.0, 0.5, 1.0) / 2.0 == Color(0.0, 0.25, 0.5));
+
+	std::cout << "Vec3 \"/\" operator works!\n";
+}
+
+void Color_addition_assignment_operator_test() {
+	std::cout << "Testing Color \"+=\" operator...\n";
+
+	Color c = Color(0.0, 0.5, 0.25);
+	c += Color(1.0, 0.5, 0.25);
+	assert(c == Color(1.0, 1.0, 0.5));
+
+	c = Color(0.0, 0.5, 0.25);
+	c += c;
+	assert(c == Color(0.0, 1.0, 0.5));
+
+	std::cout << "Color \"+=\" operator works!\n";
+}
+
+void Color_subraction_assignment_operator_test() {
+	std::cout << "Testing Color \"-=\" operator...\n";
+
+	Color c = Color(1.0, 0.5, 0.25);
+	c -= Color(0.0, 0.5, 0.125);
+	assert(c == Color(1.0, 0.0, 0.125));
+
+	std::cout << "Color \"-=\" operator works!\n";
+}
+
+void Color_multiplication_by_scalar_assignment_test() {
+	std::cout << "Testing Color \"*=\" operator...\n";
+
+	Color c = Color(0.0, 0.5, 0.25);
+	c *= 2.0;
+	assert(c == Color(0.0, 1.0, 0.5));
+
+	std::cout << "Color \"*=\" operator...\n";
+}
+
+void Color_division_by_scalar_assignment_test() {
+	std::cout << "Testing Color \"/=\" operator...\n";
+
+	Color c = Color(0.0, 0.5, 0.25);
+	c /= 2.0;
+	assert(c == Color(0.0, 0.25, 0.125));
+
+	std::cout << "Color \"/=\" operator...\n";
+}
 
 int main() {
 	Color_black_test();
