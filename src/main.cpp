ab027c0e39d0740eeebcf6054943e0d1ba761a73
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-08-24 23:48:14 +0200

    Use camera for main render

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <limits>
 
+#include "utils.hpp"
 #include "vec3.hpp"
-#include "color.hpp"
 #include "ray3.hpp"
+#include "color.hpp"
+#include "camera.hpp"
 #include "ray3hittable.hpp"
 #include "ray3hittable_list.hpp"
 #include "sphere.hpp"
+#include "random.hpp"
 
 Color ray_color(const Ray3 &ray, const Ray3Hittable &objects);
 void write_color_ppm(std::ostream &out, Color color);
@@ -14,36 +17,32 @@ void write_color_ppm(std::ostream &out, Color color);
 int main() {
 	// Image
 	const double aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	const int image_width = 1920;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 32;
 	
 	//Camera
-	double viewport_height = 2.0;
-	double viewport_width = aspect_ratio * viewport_height;
-	double focal_length = 1.0;
-
-	Vec3 origin = Vec3(0.0, 0.0, 0.0);
-	Vec3 horizontal = Vec3(viewport_width, 0.0, 0.0);
-	Vec3 vertical = Vec3(0.0, viewport_height, 0.0);
-	Vec3 lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
+	Camera camera(Vec3(0.0, 0.0, 0.0), 2.0 * aspect_ratio, 2.0, degToRad(35.0));
 
 	Ray3HittableList objects;
-	objects.add(make_shared<Sphere>(Vec3(0.0, 0.0, -1.0), 0.5));
-	objects.add(make_shared<Sphere>(Vec3(0.0,-100.5,-1.0), 100));
+	objects.add(make_shared<Sphere>(Vec3(0.0, 0.0, -10.0), 0.5));
+	objects.add(make_shared<Sphere>(Vec3(0.0,-100.5,-10.0), 100));
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for(int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for(int i = 0; i < image_width; ++i) {
-			double u = i / static_cast<double>(image_width - 1);
-			double v = j / static_cast<double>(image_height - 1);
+			Color pixel_color(0.0, 0.0, 0.0);
+			for(int s = 0; s < samples_per_pixel; ++s) {
+				double u = (i + Random::inRange(0.0, 1.0))/ static_cast<double>(image_width - 1);
+				double v = (j + Random::inRange(0.0, 1.0)) / static_cast<double>(image_height - 1);
 
-			Ray3 ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+				Ray3 ray = camera.getRay(u, v);
+				pixel_color += ray_color(ray, objects);
+			}
 
-			Color pixel_color = ray_color(ray, objects);
-
-			write_color_ppm(std::cout, pixel_color);
+			write_color_ppm(std::cout, pixel_color / static_cast<double>(samples_per_pixel));
 		}
 	}
 
@@ -63,6 +62,8 @@ Color ray_color(const Ray3 &ray, const Ray3Hittable &objects) {
 }
 
 void write_color_ppm(std::ostream &out, Color color) {
+	color = color.clamped();
+
 	out 	<< static_cast<int>(255.999 * color.getR()) << ' '
 		<< static_cast<int>(255.999 * color.getG()) << ' '
 		<< static_cast<int>(255.999 * color.getB()) << '\n';
