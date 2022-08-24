6c4f34477da9434e8204cfa2f0acfb265d937adb
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-08-24 17:26:06 +0200

    Implement sky image generation on main

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,38 @@
 
 #include "vec3.hpp"
 #include "color.hpp"
+#include "ray3.hpp"
 
+Color ray_color(const Ray3 &ray);
 void write_color_ppm(std::ostream &out, Color color);
 
 int main() {
-	const int image_width = 256;
-	const int image_height = 256;
+	// Image
+	const double aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	
+	//Camera
+	double viewport_height = 2.0;
+	double viewport_width = aspect_ratio * viewport_height;
+	double focal_length = 1.0;
+
+	Vec3 origin = Vec3(0.0, 0.0, 0.0);
+	Vec3 horizontal = Vec3(viewport_width, 0.0, 0.0);
+	Vec3 vertical = Vec3(0.0, viewport_height, 0.0);
+	Vec3 lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for(int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for(int i = 0; i < image_width; ++i) {
-			Color pixel_color(
-				i / static_cast<double>(image_width - 1),
-				j / static_cast<double>(image_height - 1),
-				0.5
-			);
+			double u = i / static_cast<double>(image_width - 1);
+			double v = j / static_cast<double>(image_height - 1);
+
+			Ray3 ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+
+			Color pixel_color = ray_color(ray);
 
 			write_color_ppm(std::cout, pixel_color);
 		}
@@ -27,6 +42,11 @@ int main() {
 	std::cerr << "\nDone.\n" << std::flush;
 
 	return 0;
+}
+
+Color ray_color(const Ray3 &ray) {
+	double t = 0.5 * (ray.getDirection().getY() + 1.0);
+	return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
 }
 
 void write_color_ppm(std::ostream &out, Color color) {
