a697496bf4119458760274dd75b30b154d5cb6b6
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-08-24 18:08:15 +0200

    Add Ray3Hittable interface

---
+++ include/ray3hittable.hpp
@@ -0,0 +1,18 @@
+#ifndef RAY3_HITTABLE_H
+#define RAY3_HITTABLE_H
+
+#include "vec3.hpp"
+#include "ray3.hpp"
+
+struct Ray3HitRecord {
+	Vec3 position;
+	Vec3 normal;
+	double distance;
+};
+
+class Ray3Hittable {
+	public:
+		virtual bool hit(const Ray3 &ray, double minDist, double maxDist, Ray3HitRecord &hitRecord) const = 0;
+};
+
+#endif
