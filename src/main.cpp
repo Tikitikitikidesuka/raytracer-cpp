e0ccce1032f6a65dc7c4413d05d368248bc2f1fe
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-08-24 19:09:43 +0200

    Use Ray3HittableList in main to render basic scene

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include "color.hpp"
 #include "ray3.hpp"
 #include "ray3hittable.hpp"
+#include "ray3hittable_list.hpp"
 #include "sphere.hpp"
 
-Color ray_color(const Ray3 &ray);
+Color ray_color(const Ray3 &ray, const Ray3Hittable &objects);
 void write_color_ppm(std::ostream &out, Color color);
 
 int main() {
@@ -25,6 +26,10 @@ int main() {
 	Vec3 vertical = Vec3(0.0, viewport_height, 0.0);
 	Vec3 lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
 
+	Ray3HittableList objects;
+	objects.add(make_shared<Sphere>(Vec3(0.0, 0.0, -1.0), 0.5));
+	objects.add(make_shared<Sphere>(Vec3(0.0,-100.5,-1.0), 100));
+
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for(int j = image_height - 1; j >= 0; --j) {
@@ -35,7 +40,7 @@ int main() {
 
 			Ray3 ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
 
-			Color pixel_color = ray_color(ray);
+			Color pixel_color = ray_color(ray, objects);
 
 			write_color_ppm(std::cout, pixel_color);
 		}
@@ -46,12 +51,11 @@ int main() {
 	return 0;
 }
 
-Color ray_color(const Ray3 &ray) {
-	Sphere sphere(Vec3(0.0, 0.0, -1.0), 0.5);
-
+Color ray_color(const Ray3 &ray, const Ray3Hittable &objects) {
 	Ray3HitRecord hitRecord;
-	if(sphere.hit(ray, 0.0001, 100000.0, hitRecord))
-		return Color(1.0, 0.0, 0.0);
+	if(objects.hit(ray, 0.0001, std::numeric_limits<double>::infinity(), hitRecord)) {
+		return Color(0.5 * (hitRecord.normal + Color::white().toVec3()));
+	}
 
 	double t = 0.5 * (ray.getDirection().getY() + 1.0);
 	return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
