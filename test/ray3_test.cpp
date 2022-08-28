c4b09b605b193d2041a76d89c504b30c3bcf6af6
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-08-28 18:33:11 +0200

    Move ray3 module's tests to gtest

--- test/ray3_test.cpp
+++ test/ray3_test.cpp
@@ -1,42 +1,29 @@
-#include <iostream>
-#include <cassert>
+#include "gtest/gtest.h"
 
 #include "ray3.hpp"
+#include "vec3.hpp"
 
-void Ray3_set_direction_test() {
-	std::cout << "Testing Ray3 direction setter...\n";
-
-	// Ray3 direction setter should normalize before setting
-	
+TEST(Ray3Test, SetDirectionNormalizes) {
 	Ray3 r;
-
+	
 	r.setDirection(Vec3(1.0, 1.0, 1.0));
-	assert(r.getDirection() == Vec3(1.0, 1.0, 1.0).normalized());
+	EXPECT_EQ(r.getDirection(), Vec3(1.0, 1.0, 1.0).normalized());
 
 	r.setDirection(Vec3(0.1, 0.1, 0.1));
-	assert(r.getDirection() == Vec3(0.1, 0.1, 0.1).normalized());
-	
-
-	std::cout << "Ray3 direction setter works!\n";
+	EXPECT_EQ(r.getDirection(), Vec3(0.1, 0.1, 0.1).normalized());
 }
 
-void Ray3_at_test() {
-	std::cout << "Testing Ray3 at...\n";
-
+TEST(Ray3Test, RayAt) {
 	Ray3 r;
 
 	r = Ray3(Vec3(0.0, 0.0, 0.0), Vec3(0.0, 1.0, 0.0));
-	assert(r.at(4.0) == Vec3(0.0, 4.0, 0.0));
+	EXPECT_EQ(r.at(4.0), Vec3(0.0, 4.0, 0.0));
 
-	// Ray3 should normalize direction automatically
 	r = Ray3(Vec3(3.59, 9.23, 19.32), Vec3(93.4, 123.2, 84.4));
-	assert(r.at(2.43).distanceTo(Vec3(4.878, 10.929, 20.484)) < 0.001);
-
-	std::cout << "Ray3 at works!\n";
+	EXPECT_LT(r.at(2.43).distanceTo(Vec3(4.878, 10.929, 20.484)), 0.001);
 }
 
-int main() {
-	Ray3_set_direction_test();
-	Ray3_at_test();
-	return 0;
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
